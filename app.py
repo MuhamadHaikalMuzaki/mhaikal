38a65d7e1b1a2140abbb6f99ce96d3bf7c3608ae
MuhamadHaikalMuzaki <hmzmuzaki@gmail.com>
2025-01-07 20:13:59 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,48 +1,96 @@
-import streamlit as st 
+import streamlit as st
 
-# CSS to Streamlit
-
+# Custom CSS untuk tampilan
 st.markdown(
     """
     <style>
-    .stApp{
-        background-color: #FFFBE6;
+    /* Warna latar belakang aplikasi */
+    .stApp {
+        background-color: #fffff;
     }
-    
-    [data-testid="stSidebar"]{
-        background-color: #347928;
+
+    /* Sidebar styling */
+    [data-testid="stSidebar"] {
+        background-color: #31511E;
         color: white;
+        padding: 5px;
     }
-    
+
+    /* Teks pada sidebar */
     [data-testid="stSidebar"] * {
         color: white !important;
-        font-size: 14px;
-    
+        font-size: 16px;
+        margin-bottom: 5px;
+        margin-top: 5px;
     }
-    
-    # .stApp{
-    # background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-    # background-size: cover;
-    # }
+
+    [data-testid="stSidebar"] *:hover {
+        color: #ecf0f1 !important;
+    }
+
+    /* Header bar styling */
+    header[data-testid="stHeader"] {
+        background-color: #859F3D;
+        color: white;
+        padding: 5px;
+        
+    }
+
+    header[data-testid="stHeader"] h1 {
+        color: white;
+        font-size: 10px;
+    }
+
+    .stButton > button:hover {
+        background-color: #2980b9;
+        color: #ecf0f1;
+    }
+
+    /* Custom title style for Kas Masjid */
+    .sidebar-title {
+        font-size: 20px;
+        font-weight: bold;
+        color: #ffffff;
+        margin-bottom: 20px;
+        text-align: center;
+    }
     
     </style>
     """,
-    unsafe_allow_html = True 
+    unsafe_allow_html=True
 )
 
-dashboard = st.Page("./fitur/dashboard.py", title="Dashboard")
-nabung = st.Page("./fitur/nabung.py", title="Menabung")
-penarikan = st.Page("./fitur/penarikan.py", title="Penarikan")
 
+#Data in-memory storage (simulasi database)
+if 'transaksi' not in st.session_state: # jika tidak ada data, maka buat data dummy
+    st.session_state['transaksi'] = [] # inisiasi data transaksi
+if 'logistik' not in st.session_state: # jika tidak ada data, maka buat data dummy
+    st.session_state['logistik'] = [] # inisiasi data logistik
+
+#Fungsi untuk menghitung total pemasukan, pengeluaran, dan saldo
+def total(transaksi): # fungsi untuk menghitung total pemasukan, pengeluaran, dan saldo
+    total_pemasukan = sum(t['jumlah'] for t in transaksi if t['type'] == 'Pemasukan') # total pemasukan
+    total_pengeluaran = sum(t['jumlah'] for t in transaksi if t['type'] == 'Pengeluaran') # total pengeluaran
+    saldo = total_pemasukan - total_pengeluaran # saldo
+    return total_pemasukan, total_pengeluaran, saldo # return total pemasukan, pengeluaran, dan saldo
+
+
+
+
+# Setiap halaman di aplikasi didefinisikan menggunakan (st.Page(path, title))
+dashboard = st.Page("./fitur/dashboard.py", title="Dashboard", icon=":material/dashboard:")
+pemasukan = st.Page("./fitur/pemasukan.py", title="Pemasukan")
+pengeluaran = st.Page("./fitur/pengeluaran.py", title="Pengeluaran")
+logistik = st.Page("./fitur/logistik.py", title="Logistik")
+riwayat = st.Page("./fitur/riwayat.py", title="Riwayat", icon=":material/history:")
+
+# Membuat navigasi halaman dalam aplikasi.
 pg = st.navigation(
     {
-     "Menu Utama" : [dashboard],
-     "Transaksi" : [nabung, penarikan],
-        
+    "Rekap": [dashboard, riwayat],
+    "Utama": [pemasukan,pengeluaran, logistik],
     }
 )
 
-if 'total_semua' not in st.session_state:
-    st.session_state['total_semua'] = []
-
+# Menjalankan navigasi dan memuat halaman sesuai pilihan pengguna.
 pg.run()
---
+++ dashboardd.py
@@ -0,0 +1,18 @@
+import streamlit as st 
+from app import total
+
+
+
+#Halaman Dashboard
+st.title("Aplikasi Masjid Nurul Jami") # Menampilkan Judul
+st.markdown("Selamat Datang di Aplikasi Masjid Nurul Jami") # Menampilkan Deskripsi
+st.image("Images.jpg", caption="Masjid Nurul Jami") # Menampilkan Gambar dengan caption
+    
+    
+st.title("Dashboard Kas Masjid") # Menampilkan Judul
+transaksi = st.session_state['transaksi'] # Mengambil data transaksi dari st.session_state
+total_pemasukan, total_pengeluaran, saldo = total(transaksi) # Memanggil fungsi total dari tiga nilai tersebut.
+
+st.metric("Total Pemasukan", f"Rp {total_pemasukan:,}") # Menampilkan metrik berupa total pemasukan dalam format rupiah
+st.metric("Total Pengeluaran", f"Rp {total_pengeluaran:,}") # Menampilkan metrik berupa total pengeluaran dalam format rupiah
+st.metric("Saldo Kas", f"Rp {saldo:,}") # Menampilkan metrik saldo kas (total pemasukan dikurangi total pengeluaran)
